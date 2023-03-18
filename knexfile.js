7b6e14d23072963dafd02e3f16c700c463177f5f
Thiago Pereira <thiagopereiramail@gmail.com>
2023-03-18 15:19:54 -0300

    feat: Configurar o banco de desenvolvimento e testes

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations/dev',
+    },
+    seeds: {
+      directory: './migrations/dev/seeds',
+    },
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3',
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations/dev',
+    },
+    seeds: {
+      directory: './migrations/dev/seeds',
+    },
+  },
+};
