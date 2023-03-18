dcbc8fc54797890d4b441dfdea1a5f937c98cfdd
Thiago Pereira <thiagopereiramail@gmail.com>
2023-03-18 18:25:57 -0300

    feat: Configurar a conexão banco de desenvolvimento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: process.env.DIALECT as any,
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT),
+      database: process.env.DATABASE,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
