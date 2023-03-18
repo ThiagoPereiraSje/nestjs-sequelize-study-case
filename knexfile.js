6a5c9df7246038f1f6a4bf52d117ecc3729dbc4f
Thiago Pereira <thiagopereiramail@gmail.com>
2023-03-18 16:06:22 -0300

    feat: Configurar banco de dados de teste

--- knexfile.js
+++ knexfile.js
@@ -27,9 +27,17 @@ module.exports = {
   },
 
   test: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3',
+      host: 'localhost',
+      port: 5433,
+      database: 'test_db',
+      user: 'postgres',
+      password: 'postgres',
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
       tableName: 'knex_migrations',
