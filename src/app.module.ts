88e0cccb18ca2f42c93ea601b2ed0009cdca0892
Thiago Pereira <thiagopereiramail@gmail.com>
2023-03-19 15:35:13 -0300

    feat: Configurar a conexão do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { join } from 'path';
-
-import models from './models';
+import { QueriesModule } from './queries/queries.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    SequelizeModule.forRoot({
-      dialect: process.env.DIALECT as any,
-      host: process.env.HOST,
-      port: parseInt(process.env.PORT),
-      database: process.env.DATABASE,
-      username: process.env.USERNAME,
-      password: process.env.PASSWORD,
-      models: [...models],
-    }),
+    DatabaseModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
@@ -31,6 +22,7 @@ import models from './models';
         outputAs: 'class',
       },
     }),
+    QueriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/database/database.providers.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from 'sequelize-typescript';
+
+export const databaseProviders = [
+  {
+    provide: 'DEV_DB',
+    useFactory: async () => {
+      const dirname = __dirname.split('/database')[0];
+
+      const db = new Sequelize({
+        dialect: process.env.DIALECT as any,
+        host: process.env.HOST,
+        port: parseInt(process.env.PORT),
+        database: process.env.DATABASE,
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        models: [`${dirname}/models`],
+      });
+
+      return db;
+    },
+  },
+];
